b51d2243de0eb11343778b92df15d7c85536540d
Ubuntu <ubuntu@ip-172-31-95-182.ec2.internal>
2023-07-02 18:26:43 +0000

    delete unnecessary

---
+++ run_kubernetes.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+docker_path=vuvm1
+
+# Step 2
+kubectl create deploy project-ml-microservice-kubernetes --image="$docker_path/project-ml:v1.0.2"
+
+
+# Step 3:
+# List kubernetes pods
+kubectl get pods
+
+# Step 4:
+# Forward the container port to a host
+kubectl port-forward deployment.apps/project-ml-microservice-kubernetes 8000:80
