02ff7c59d5c21488cb29a39b4d789060e46d0999
minhvu2403 <50311217+minhvu2403@users.noreply.github.com>
2023-07-03 02:01:13 +0700

    Update upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -17,14 +17,19 @@ docker login
 image_tagged=$(docker image list --filter=reference="$docker_path/project-ml" | grep 'project-ml' | xargs)
 if [[ -n $image_tagged ]]; then
   echo "Image already tagged, remove the tagged image."
+  # Extract the image name from the tagged image information
   name=$(echo "$image_tagged" | cut -f 1 -d " ")
+  # Extract the image tag from the tagged image information
   tag=$(echo "$image_tagged" | cut -f 2 -d " ")
+  # Remove the tagged image forcefully
   docker image remove --force "$name":"$tag"
 fi
 
 # Get information about the image by substring 
 image_info=$(docker image list | grep 'project-ml' | xargs)
+# Extract the image name from the image information
 image_name=$(echo "$image_info" | cut -f 1 -d " ")
+# Extract the image tag from the image information
 image_tag=$(echo "$image_info" | cut -f 2 -d " ")
 
 # Tag the image with the specified repository and tag
